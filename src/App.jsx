a12f095da7816641219326a541e64fa64b88d71f
Babar <babarjawad010@gmail.com>
2022-11-14 16:47:14 +0500

    Alert commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
-import Services from './components/services/Services'
-import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/testimonials/Testimonials'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
+import React, { useState } from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Alert from "./components/alert/Alert";
 
 const App = () => {
+  const [alert, setAlert] = useState({ show: false });
+
+  const handleALert = () => {
+    setAlert({ show: true });
+    setTimeout(() => {
+      setAlert({ show: false });
+    }, 2000);
+  };
   return (
     <>
-
+      {alert.show && <Alert />}
       <Header />
       <Nav />
       <About />
@@ -20,11 +29,10 @@ const App = () => {
       <Services />
       <Portfolio />
       <Testimonials />
-      <Contact />
+      <Contact handleALert={handleALert} />
       <Footer />
-
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/alert/Alert.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Alert.css";
+
+function Alert() {
+  return (
+    <div className="cont">
+      <div className="alert">Email has been sent successfully!</div>
+    </div>
+  );
+}
+
+export default Alert;
